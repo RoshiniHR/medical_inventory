91f3c6d7d419e51f4e68f734f4da6e34d418d2e4
RoshiniHR <137640885+RoshiniHR@users.noreply.github.com>
2025-06-02 23:19:12 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -45,6 +45,51 @@ if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
+def load_images_and_labels(img_dir, label_csv):
+    df = pd.read_csv(label_csv)
+    images, labels = [], []
+
+    for _, row in df.iterrows():
+        img_path = os.path.join(img_dir, row['image_name'])
+        img = cv2.imread(img_path)
+        if img is None:
+            continue
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, IMAGE_SIZE)
+        img = img / 255.0
+        images.append(img)
+        labels.append(int(row['label']))
+
+    return np.array(images), to_categorical(labels, num_classes=3)
+
+def create_model():
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(*IMAGE_SIZE, 3)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.4),
+        layers.Dense(3, activation='softmax')  # 3 classes
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+def train():
+    images, labels = load_images_and_labels('images/', 'labels.csv')
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+    model = create_model()
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_test, y_test))
+    model.save('pollution_level_model.keras')
+
+if __name__ == '__main__':
+    train()
 
 
 
@@ -73,6 +118,10 @@ if __name__ == '__main__':
 
 
 
+
+
+
+sudo gedit /var/lib/jenkins/workspace/AnsiDeploy/hosts.ini
 
 pipeline
 {
@@ -94,7 +143,6 @@ pipeline
                 sh 'mvn clean package'
             }       
         }
-    
         stage("Test")
         {
             steps
@@ -102,8 +150,6 @@ pipeline
                 sh 'mvn test'
             }
         }
-    
-
         stage("Archive Artifacts")
         {
             steps
@@ -111,7 +157,6 @@ pipeline
                 archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
             }   
         }
-    
         stage("Deploy")
         {
             steps
